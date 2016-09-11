660ac5d7932d8e6cb3ac677b7b837e2b0b114fc3
Blake Irwin <blake@proxycha.in>
2016-09-12 01:49:30 +1200

    Routes

---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/**
+ *  Frequency controller
+ */
+Route::group([ 'prefix' => 'frequencies' ], function () {
+    Route::get('/{region}', 'FrequencyController@region');
+});
+
+
+/**
+ *  Genre controller
+ */
+Route::get('genres', 'GenreController@genres');
+
+
+/**
+ *  Region controller
+ */
+Route::group([ 'prefix' => 'regions' ], function () {
+    Route::get('/', 'RegionController@regions');
+    Route::get('/{region}/cities', 'RegionController@regionCities');
+    Route::get('/{region}/stations', 'RegionController@regionStations');
+});
+
+
+/**
+ *  Station controller
+ */
+Route::group([ 'prefix' => 'stations' ], function () {
+    Route::get('/', 'StationController@stations');
+    Route::get('/{station}', 'StationController@station');
+});
+
+
+/**
+ *  Statistics controller
+ */
+Route::get('statistics', 'StatisticsController@statistics');
