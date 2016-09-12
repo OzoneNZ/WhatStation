b5f2b9b07e05b242c637a3d7a90e3a1bf3419c01
Blake Irwin <blake@proxycha.in>
2016-09-12 22:18:34 +1200

    Remove erroneous <base> tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 
-        <!-- Application base -->
-        <base href="/app">
-
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="/images/ws-logo.png" />
 
